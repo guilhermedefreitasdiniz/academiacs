51d8706f5c8773ec3eafd07c0d074787876d0d08
Guilherme <guilhermedefreitasdiniz@gmail.com>
2026-08-17 14:44:06 -0300

    SSL ativo no banco e caixinhas do PIN estreitas

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const { Pool } = require('pg');
 const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
+
 
 const app = express();
 
